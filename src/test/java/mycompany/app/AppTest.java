d9ae9d16669376ab1a8a72da98a423d7cf4a5ce5
Lutfee <mynameislutfee@gmail.com>
2022-11-14 23:54:14 +0800

    sial

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://localhost";
+	String url = "192.168.86.27";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
